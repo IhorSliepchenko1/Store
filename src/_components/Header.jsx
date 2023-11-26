61933df5b7ac3534a07f13c719f406c3dfcef3a8
IhorSliepchenko1 <igorslepchenko.cc@gmail.comD>
2023-11-26 21:41:09 +0200

    header & footer mobile adaptive

--- src/_components/Header.jsx
+++ src/_components/Header.jsx
@@ -1,63 +1,88 @@
+import { useState } from "react";
+import DensityMediumTwoToneIcon from "@mui/icons-material/DensityMediumTwoTone";
+import ClearTwoToneIcon from "@mui/icons-material/ClearTwoTone";
 import Logo from "../assets/images/logo.svg";
 import Search from "../assets/images/search.svg";
 import Heart from "../assets/images/heart.svg";
 import Shop from "../assets/images/shop.svg";
 
 export const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const navToggle = () => {
+    setIsNavOpen(!isNavOpen);
+    document.getElementById("headerUl").classList.toggle("_active");
+    document.getElementById("headerBackdrop").classList.toggle("_active");
+    document.getElementById("header-burger").classList.toggle("_active");
+    document.body.classList.toggle("no-scroll");
+  };
+
   return (
     <>
-      <header className="header">
+      <header className={`header ${isNavOpen ? "header--open" : ""}`}>
         <a href="#" className="header__logo">
           <img src={Logo} alt="Logo" className="header__img-logo" />
         </a>
 
         <nav className="header__nav">
-          <ul className="header__ul">
-            <a href="#" className="header__a _active">
-              home
-            </a>
-          </ul>
-          <ul className="header__ul">
-            <a href="#" className="header__a">
-              Shop
-            </a>
-          </ul>
-          <ul className="header__ul">
-            <a href="#" className="header__a">
-              blog
-            </a>
-          </ul>
-          <ul className="header__ul">
-            <a href="#" className="header__a">
-              Sale
-            </a>
-          </ul>
-          <ul className="header__ul">
-            <a href="#" className="header__a">
-              contact us
-            </a>
-          </ul>
-          <ul className="header__ul">
-            <div className="header__ul-search">
-              <img src={Search} alt="search" className="header__ul-img" />
-            </div>
-            <a href="#" className="header__a">
-              search
-            </a>
+          <div
+            className={`header__backdrop ${isNavOpen ? "_active" : ""}`}
+            onClick={navToggle}
+          ></div>
+
+          <ul className={`header__ul ${isNavOpen ? "_active" : ""}`}>
+            <ClearTwoToneIcon id="header-close" onClick={navToggle} />
+
+            <li className="header__li">
+              <a href="#" className="header__a _active">
+                home
+              </a>
+            </li>
+            <li className="header__li">
+              <a href="#" className="header__a">
+                Shop
+              </a>
+            </li>
+            <li className="header__li">
+              <a href="#" className="header__a">
+                blog
+              </a>
+            </li>
+            <li className="header__li">
+              <a href="#" className="header__a">
+                Sale
+              </a>
+            </li>
+            <li className="header__li">
+              <a href="#" className="header__a">
+                contact us
+              </a>
+            </li>
+
+            <li className="header__li search-li">
+              <div className="header__li-search">
+                <img src={Search} alt="search" className="header__li-img" />
+              </div>
+
+              <a href="#" className="header__a">
+                search
+              </a>
+            </li>
           </ul>
         </nav>
 
         <div className="header__info">
+          <DensityMediumTwoToneIcon id="header-burger" onClick={navToggle} />
           <div className="header__reg">
             <ul className="header__ul-reg">
               <li className="header__li">
-                <a href="#" className="header__a">
+                <a href="#" className="header__a a-reg">
                   SIGN IN
                 </a>
               </li>
 
               <li className="header__li">
-                <a href="#" className="header__a">
+                <a href="#" className="header__a a-reg">
                   CREATE AN ACCOUNT
                 </a>
               </li>
